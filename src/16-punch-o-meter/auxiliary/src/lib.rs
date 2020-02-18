eb79706f8663969f7f3b70743bc062b6045cb8ad
aaqib-kk <aqib.khan.25@iiee.edu.pk>
2020-02-18 21:18:34 +0500

    updating discovery repo MAK

--- src/16-punch-o-meter/auxiliary/src/lib.rs
+++ src/16-punch-o-meter/auxiliary/src/lib.rs
@@ -1,21 +1,156 @@
-//! Initialization code
+// //  origial lib
+
+// #![no_std]
+
+// #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
+// extern crate panic_itm; // panic handler
+//                         // extern crate panic_semihosting;
+
+// pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
+// pub use cortex_m_rt::entry;
+// pub use f3::{
+//     hal::{delay::Delay, prelude, spi::Spi, time::MonoTimer},
+//     l3gd20,
+//     l3gd20::I16x3 as OtherI16x3,
+//     lsm303dlhc::{I16x3, Sensitivity},
+//     L3gd20, Lsm303dlhc,
+// };
+
+// pub use f3::hal::stm32f30x::{gpioc, rcc};
+
+// use f3::hal::{
+//     i2c::I2c,
+//     prelude::*,
+//     stm32f30x::{self, GPIOE, RCC},
+// };
+
+// pub fn init() -> (L3gd20, Lsm303dlhc, Delay, MonoTimer, ITM) {
+//     let cp = cortex_m::Peripherals::take().unwrap();
+//     let dp = stm32f30x::Peripherals::take().unwrap();
+
+//     let mut flash = dp.FLASH.constrain();
+//     let mut rcc = dp.RCC.constrain();
+
+//     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+//     let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+//     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+//     let mut nss = gpioe
+//         .pe3
+//         .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+//     nss.set_high();
+
+//     let sck = gpioa.pa5.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+//     let miso = gpioa.pa6.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+//     let mosi = gpioa.pa7.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+
+//     let spi = Spi::spi1(
+//         dp.SPI1,
+//         (sck, miso, mosi),
+//         l3gd20::MODE,
+//         1.mhz(),
+//         clocks,
+//         &mut rcc.apb2,
+//     );
+//     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+//     let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+//     let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+
+//     let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
+//     let mut l3gd20 = L3gd20::new(spi, nss).unwrap();
+//     let lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
+
+//     assert_eq!(l3gd20.who_am_i().unwrap(), 0xD4);
+
+//     let delay = Delay::new(cp.SYST, clocks);
+//     let mono_timer = MonoTimer::new(cp.DWT, clocks);
+
+//     (l3gd20, lsm303dlhc, delay, mono_timer, cp.ITM)
+// }
+
+//=============================== Version 2 =======================
+
+// #![no_std]
+
+// #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
+// extern crate panic_itm; // panic handler
+
+// pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
+// pub use cortex_m_rt::entry;
+// pub use f3::{
+//     hal::{delay::Delay, prelude, time::MonoTimer},
+//     lsm303dlhc::{I16x3, Sensitivity},
+//     Lsm303dlhc,
+// };
+
+// use f3::hal::{i2c::I2c, prelude::*, stm32f30x};
+
+// pub fn init() -> (Lsm303dlhc, Delay, MonoTimer, ITM) {
+//     let cp = cortex_m::Peripherals::take().unwrap();
+//     let dp = stm32f30x::Peripherals::take().unwrap();
+
+//     let mut flash = dp.FLASH.constrain();
+//     let mut rcc = dp.RCC.constrain();
+
+//     let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+//     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+//     let mut nss = gpioe
+//         .pe3
+//         .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+//     nss.set_high();
+
+//     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+//     let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+//     let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+
+//     let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
+//     let lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
+
+//     let delay = Delay::new(cp.SYST, clocks);
+//     let mono_timer = MonoTimer::new(cp.DWT, clocks);
+
+//     (lsm303dlhc, delay, mono_timer, cp.ITM)
+// }
+
+// ================= Making for output =================
+//  origial lib
 
 #![no_std]
 
 #[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
 extern crate panic_itm; // panic handler
+                        // extern crate panic_semihosting;
 
 pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
 pub use cortex_m_rt::entry;
 pub use f3::{
-    hal::{delay::Delay, prelude, time::MonoTimer},
+    hal::{delay::Delay, prelude, spi::Spi, time::MonoTimer},
+    l3gd20,
+    l3gd20::I16x3 as OtherI16x3,
     lsm303dlhc::{I16x3, Sensitivity},
-    Lsm303dlhc,
+    L3gd20, Lsm303dlhc,
 };
 
-use f3::hal::{i2c::I2c, prelude::*, stm32f30x};
+pub use f3::hal::stm32f30x::{gpioc, rcc};
 
-pub fn init() -> (Lsm303dlhc, Delay, MonoTimer, ITM) {
+use f3::hal::{
+    i2c::I2c,
+    prelude::*,
+    stm32f30x::{self, GPIOE, RCC},
+};
+
+pub fn init() -> (
+    &'static gpioc::RegisterBlock,
+    &'static rcc::RegisterBlock,
+    L3gd20,
+    Lsm303dlhc,
+    Delay,
+    MonoTimer,
+    ITM,
+) {
     let cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32f30x::Peripherals::take().unwrap();
 
@@ -24,22 +159,52 @@ pub fn init() -> (Lsm303dlhc, Delay, MonoTimer, ITM) {
 
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
     let mut nss = gpioe
         .pe3
         .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
     nss.set_high();
+    //===================================
+    // let mut ledd9 = gpioe
+    //     .pe9
+    //     .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+    // ledd9.set_high();
+    //===================================
+    let sck = gpioa.pa5.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+    let miso = gpioa.pa6.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+    let mosi = gpioa.pa7.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
 
+    let spi = Spi::spi1(
+        dp.SPI1,
+        (sck, miso, mosi),
+        l3gd20::MODE,
+        1.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
     let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
     let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
 
     let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
 
+    let mut l3gd20 = L3gd20::new(spi, nss).unwrap();
+    // let mut green = ledd9;
     let lsm303dlhc = Lsm303dlhc::new(i2c).unwrap();
+
+    assert_eq!(l3gd20.who_am_i().unwrap(), 0xD4);
 
     let delay = Delay::new(cp.SYST, clocks);
     let mono_timer = MonoTimer::new(cp.DWT, clocks);
 
-    (lsm303dlhc, delay, mono_timer, cp.ITM)
+    (
+        unsafe { &*GPIOE::ptr() },
+        unsafe { &*RCC::ptr() },
+        l3gd20,
+        lsm303dlhc,
+        delay,
+        mono_timer,
+        cp.ITM,
+    )
 }
--- src/16-punch-o-meter/src/main.rs
+++ src/16-punch-o-meter/src/main.rs
@@ -1,17 +1,31 @@
+//// This code is working for angle measurements =============================
+
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 #[allow(unused_imports)]
-use aux16::{entry, iprint, iprintln, prelude::*, I16x3, Sensitivity};
+use aux16::{entry, gpioc, iprint, iprintln, prelude::*, I16x3, OtherI16x3, Sensitivity};
+use core::f32::consts::PI;
+use m::Float;
 
 #[entry]
 fn main() -> ! {
-    let (mut lsm303dlhc, mut delay, _mono_timer, mut itm) = aux16::init();
+    let (gpioe, rcc, mut l3gd20, mut lsm303dlhc, mut delay, _mono_timer, mut itm) = aux16::init();
 
     // extend sensing range to `[-12g, +12g]`
     lsm303dlhc.set_accel_sensitivity(Sensitivity::G12).unwrap();
     loop {
+
+        //=============== Gyroscope=======================
+        // let _m = l3gd20.all().unwrap();
+        // iprintln!(&mut itm.stim[0], "{:?}", _m);
+        let OtherI16x3 { x, y, z } = l3gd20.gyro().unwrap();
+        let mut xx = x as f32;
+
+        // iprintln!(&mut itm.stim[0], "{:?}", xx);
+
+        //================= Accelerometer==================
         const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
 
         let I16x3 { x, y, z } = lsm303dlhc.accel().unwrap();
@@ -20,8 +34,360 @@ fn main() -> ! {
         let y = f32::from(y) * SENSITIVITY;
         let z = f32::from(z) * SENSITIVITY;
 
-        iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
+        // iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
 
-        delay.delay_ms(1_000_u16);
+        //============ Converting to degree of rotation ===========
+        let mut fxg = x as f32 * 0.04 as f32;
+        let mut fyg = y as f32 * 0.04 as f32;
+        let mut fzg = z as f32 * 0.04 as f32;
+        // let mut fig = i;
+
+        let xg: f32 = 0.;
+        let mut x = fxg * 0.63 as f32 + (xg * (1.0 - 0.63) as f32);
+        let mut xg = x;
+        let yg: f32 = 0.;
+        let mut y = fyg * 0.63 as f32 + (yg * (1.0 - 0.63) as f32);
+        let mut yg = y;
+        let zg: f32 = 0.;
+        let mut z = fzg * 0.63 as f32 + (zg * (1.0 - 0.63) as f32);
+        let mut zg = z;
+
+        let x = x as f32;
+        let y = y as f32;
+
+        let mut pitch = (((y * y + z * z).sqrt()).atan2(x) * 180.0) / PI;
+        let mut pgyro = xx * 0.00005;
+
+        let CFILTIER_COEF = 0.95;
+        let mut angle: f32 = -4.7;
+        let angle = ((CFILTIER_COEF) * ((angle) + pgyro) + (1.0 - CFILTIER_COEF) * (pitch));
+        let mut modified_angle = angle * 20.0;
+        if modified_angle > 0. {
+            iprintln!(
+                &mut itm.stim[0],
+                "Turning Angle Clockwise = {:?} ",
+                modified_angle
+            );
+        } else {
+            iprintln!(
+                &mut itm.stim[0],
+                "Turning Angle CounterClockwise = {:?} ",
+                modified_angle
+            );
+        }
+
+        //         //enable GPIOE peripheral
+        rcc.ahbenr.modify(|_, w| w.iopeen().set_bit());
+
+        //configure the pins asoutputs
+        gpioe.moder.modify(|_, w| {
+            w.moder8().output();
+            w.moder9().output();
+            w.moder10().output();
+            w.moder11().output();
+            w.moder12().output();
+            w.moder13().output();
+            w.moder14().output();
+            w.moder15().output()
+        });
+
+        if modified_angle > 10.0 && modified_angle < 25.0 {
+            gpioe.odr.write(|w| {
+                w.odr8().set_bit();
+                w.odr9().set_bit()
+            });
+        } else if modified_angle > 25.0 && modified_angle < 40.0 {
+            gpioe.odr.write(|w| {
+                w.odr8().set_bit();
+                w.odr9().set_bit();
+                w.odr10().set_bit();
+                w.odr11().set_bit()
+            });
+        } else if modified_angle > 40.0 && modified_angle < 55.0 {
+            gpioe.odr.write(|w| {
+                w.odr8().set_bit();
+                w.odr9().set_bit();
+                w.odr10().set_bit();
+                w.odr11().set_bit();
+                w.odr12().set_bit();
+                w.odr13().set_bit()
+            });
+        } else if modified_angle > 55.0 && modified_angle < 70.0 {
+            gpioe.odr.write(|w| {
+                w.odr8().set_bit();
+                w.odr9().set_bit();
+                w.odr10().set_bit();
+                w.odr11().set_bit();
+                w.odr12().set_bit();
+                w.odr13().set_bit();
+                w.odr14().set_bit();
+                w.odr15().set_bit()
+            });
+        }
+        //=============== reversing =================
+        else if modified_angle < -10.0 && modified_angle > -25.0 {
+            gpioe.odr.write(|w| {
+                w.odr15().set_bit();
+                w.odr14().set_bit()
+            });
+        } else if modified_angle < -25.0 && modified_angle > -40.0 {
+            gpioe.odr.write(|w| {
+                w.odr15().set_bit();
+                w.odr14().set_bit();
+                w.odr13().set_bit();
+                w.odr12().set_bit()
+            });
+        } else if modified_angle < -40.0 && modified_angle > -55.0 {
+            gpioe.odr.write(|w| {
+                w.odr15().set_bit();
+                w.odr14().set_bit();
+                w.odr13().set_bit();
+                w.odr12().set_bit();
+                w.odr11().set_bit();
+                w.odr10().set_bit()
+            });
+        } else if modified_angle < -55.0 && modified_angle > -70.0 {
+            gpioe.odr.write(|w| {
+                w.odr15().set_bit();
+                w.odr14().set_bit();
+                w.odr13().set_bit();
+                w.odr12().set_bit();
+                w.odr11().set_bit();
+                w.odr10().set_bit();
+                w.odr8().set_bit();
+                w.odr9().set_bit()
+            });
+        } else {
+            gpioe.odr.write(|w| {
+                w.odr8().clear_bit();
+                w.odr9().clear_bit();
+                w.odr10().clear_bit();
+                w.odr11().clear_bit();
+                w.odr12().clear_bit();
+                w.odr13().clear_bit();
+                w.odr14().clear_bit();
+                w.odr15().clear_bit()
+            });
+        }
+
+        delay.delay_ms(4_00_u16);
     }
 }
+
+// ============= version 2 ===================
+
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// #[allow(unused_imports)]
+// use aux16::{entry, iprint, iprintln, prelude::*, I16x3, Sensitivity};
+// use m::Float;
+
+// #[entry]
+// fn main() -> ! {
+//     const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
+//     const THRESHOLD: f32 = 0.5;
+
+//     let (mut lsm303dlhc, mut delay, mono_timer, mut itm) = aux16::init();
+
+//     lsm303dlhc.set_accel_sensitivity(Sensitivity::G12).unwrap();
+
+//     let measurement_time = mono_timer.frequency().0; // 1 second in ticks
+//     let mut instant = None;
+//     let mut max_g = 0.;
+//     loop {
+//         let g_x = f32::from(lsm303dlhc.accel().unwrap().x).abs() * SENSITIVITY;
+
+//         match instant {
+//             None => {
+//                 // If acceleration goes above a threshold, we start measuring
+//                 if g_x > THRESHOLD {
+//                     iprintln!(&mut itm.stim[0], "START!");
+
+//                     max_g = g_x;
+//                     instant = Some(mono_timer.now());
+//                 }
+//             }
+//             // Still measuring
+//             Some(ref instant) if instant.elapsed() < measurement_time => {
+//                 if g_x > max_g {
+//                     max_g = g_x;
+//                 }
+//             }
+//             _ => {
+//                 // Report max value
+//                 iprintln!(&mut itm.stim[0], "Max acceleration: {}g", max_g);
+
+//                 // Measurement done
+//                 instant = None;
+
+//                 // Reset
+//                 max_g = 0.;
+//             }
+//         }
+
+//         delay.delay_ms(50_u8);
+//     }
+// // }
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// #[allow(unused_imports)]
+// use aux16::{entry, iprint, iprintln, prelude::*, I16x3, Sensitivity};
+// use m::Float;
+
+// #[entry]
+// fn main() -> ! {
+//     const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
+//     const THRESHOLD: f32 = 0.5;
+
+//     let (mut lsm303dlhc, mut delay, mono_timer, mut itm) = aux16::init();
+
+//     lsm303dlhc.set_accel_sensitivity(Sensitivity::G12).unwrap();
+
+//     let measurement_time = mono_timer.frequency().0; // 1 second in ticks
+//     let mut instant = None;
+//     let mut max_g = 0.;
+//     loop {
+//         let g_x = f32::from(lsm303dlhc.accel().unwrap().x).abs() * SENSITIVITY*10.;
+
+//         match instant {
+//             None => {
+//                 // If acceleration goes above a threshold, we start measuring
+//                 if g_x > THRESHOLD {
+//                     iprintln!(&mut itm.stim[0], "START!");
+
+//                     max_g = g_x;
+//                     instant = Some(mono_timer.now());
+//                 }
+//             }
+//             // Still measuring
+//             Some(ref instant) if instant.elapsed() < measurement_time => {
+//                 if g_x > max_g {
+//                     max_g = g_x;
+//                 }
+//             }
+//             _ => {
+//                 // Report max value
+//                 iprintln!(&mut itm.stim[0], "Max acceleration: {}g", max_g);
+
+//                 // Measurement done
+//                 instant = None;
+
+//                 // Reset
+//                 max_g = 0.;
+//             }
+//         }
+
+//         delay.delay_ms(50_u8);
+//     }
+// }
+
+//============ angle measurement origial  ================
+
+// #![deny(unsafe_code)]
+// #![no_main]
+// #![no_std]
+
+// #[allow(unused_imports)]
+// use aux16::{entry, iprint, iprintln, prelude::*, I16x3, OtherI16x3, Sensitivity};
+// use core::f32::consts::PI;
+// use m::Float;
+
+// #[entry]
+// fn main() -> ! {
+//     let (mut l3gd20, mut lsm303dlhc, mut delay, _mono_timer, mut itm) = aux16::init();
+
+//     // extend sensing range to `[-12g, +12g]`
+//     lsm303dlhc.set_accel_sensitivity(Sensitivity::G12).unwrap();
+
+//     loop {
+//         //=============== Gyroscope=======================
+
+//         // let _m = l3gd20.all().unwrap();
+//         // iprintln!(&mut itm.stim[0], "{:?}", _m);
+//         let OtherI16x3 { x, y, z } = l3gd20.gyro().unwrap();
+//         let mut xx = x as f32;
+//         let mut yy = y as f32;
+
+//         // iprintln!(&mut itm.stim[0], "{:?}", xx);
+
+//         //================= Accelerometer==================
+//         const SENSITIVITY: f32 = 12. / (1 << 14) as f32;
+
+//         let I16x3 { x, y, z } = lsm303dlhc.accel().unwrap();
+
+//         let x = f32::from(x) * SENSITIVITY;
+//         let y = f32::from(y) * SENSITIVITY;
+//         let z = f32::from(z) * SENSITIVITY;
+
+//         // iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
+
+//         //============ Converting to degree of rotation ===========
+//         let mut fxg = x as f32 * 0.04 as f32;
+//         let mut fyg = y as f32 * 0.04 as f32;
+//         let mut fzg = z as f32 * 0.04 as f32;
+//         // let mut fig = i;
+
+//         let xg: f32 = 0.;
+//         let mut x = fxg * 0.63 as f32 + (xg * (1.0 - 0.63) as f32);
+//         let mut xg = x;
+//         let yg: f32 = 0.;
+//         let mut y = fyg * 0.63 as f32 + (yg * (1.0 - 0.63) as f32);
+//         let mut yg = y;
+//         let zg: f32 = 0.;
+//         let mut z = fzg * 0.63 as f32 + (zg * (1.0 - 0.63) as f32);
+//         let mut zg = z;
+
+//         let x = x as f32;
+//         let y = y as f32;
+
+//         let mut pitch = (((y * y + z * z).sqrt()).atan2(x) * 180.0) / PI;
+//         let mut pgyro = xx * 0.00005;
+//         let mut pgyroy = yy * 0.00005;
+
+//         let CFILTIER_COEF = 0.95;
+//         let mut angle: f32 = -4.7;
+//         let angle = ((CFILTIER_COEF) * ((angle) + pgyro) + (1.0 - CFILTIER_COEF) * (pitch));
+//         iprintln!(&mut itm.stim[0], "Angle = {:?}", angle * 20.0);
+
+//         //         //enable GPIOE peripheral
+//         // rcc.ahbenr.modify(|_, w| w.iopeen().set_bit());
+
+//         // //     //configure the pins asoutputs
+//         // gpioe.moder.modify(|_, w| {
+//         //     w.moder8().output();
+//         //     w.moder9().output();
+//         //     w.moder10().output();
+//         //     w.moder11().output();
+//         //     w.moder12().output();
+//         //     w.moder13().output();
+//         //     w.moder14().output();
+//         //     w.moder15().output()
+//         // });
+
+//         // gpioe.odr.write(|w| match angle {
+//         //     1.0 => w.odr1().set_bit(),
+//         //     2.0 => w.odr2().set_bit(),
+//         //     3.0 => w.odr3().set_bit(),
+//         //     4.0 => w.odr4().set_bit(),
+//         //     5.0 => w.odr5().set_bit(),
+//         //     6.0 => w.odr6().set_bit(),
+//         //     7.0 => w.odr6().set_bit(),
+//         //     8.0 => w.odr8().set_bit(),
+//         //     -1.0 => w.odr1().clear_bit(),
+//         //     -2.0 => w.odr2().clear_bit(),
+//         //     -3.0 => w.odr3().clear_bit(),
+//         //     -4.0 => w.odr4().clear_bit(),
+//         //     -5.0 => w.odr5().clear_bit(),
+//         //     -6.0 => w.odr6().clear_bit(),
+//         //     -7.0 => w.odr6().clear_bit(),
+//         //     -8.0 => w.odr8().clear_bit(),
+//         //     _ => w.odr8().clear_bit(),
+//         // });
+
+//         delay.delay_ms(4_00_u16);
+//     }
+// }
