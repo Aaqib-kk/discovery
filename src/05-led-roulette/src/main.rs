4272e1398cff895818a55d94abf11aae61a82601
aaqib-kk <aqib.khan.25@iiee.edu.pk>
2020-02-23 15:31:03 +0500

    updating for leds

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -1,99 +1,99 @@
-#![deny(unsafe_code)]
-#![no_main]
-#![no_std]
-
-use aux5::{entry, prelude::*, Delay, Leds};
-
-#[entry]
-fn main() -> ! {
-    let (mut delay, mut leds): (Delay, Leds) = aux5::init();
-
-    let ms = 50_u8;
-    loop {
-        for curr in 0..8 {
-            let next = (curr + 1) % 8;
-
-            leds[next].on();
-            delay.delay_ms(ms);
-            leds[curr].off();
-            delay.delay_ms(ms);
-        }
-    }
-}
-
-//========= using input ===================== 
+// #![deny(unsafe_code)]
 // #![no_main]
 // #![no_std]
 
-// use aux5::{entry, prelude::*};
+// use aux5::{entry, prelude::*, Delay, Leds};
 
 // #[entry]
 // fn main() -> ! {
-//     let (mut delay,gpioa, gpiob, gpioc, gpiod, gpioe, gpiof, rcc) = aux5::init();
+//     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
 
-//     rcc.ahbenr.modify(|_, w| {
-//         w.iopaen().set_bit();
-//         w.iopben().set_bit();
-//         w.iopcen().set_bit();
-//         w.iopden().set_bit();
-//         w.iopeen().set_bit();
-//         w.iopfen().set_bit()
+//     let ms = 50_u8;
+//     loop {
+//         for curr in 0..8 {
+//             let next = (curr + 1) % 8;
 
-//     });
-//     gpiob.moder.modify(|_,w| w.moder0().output());
-//     gpiob.odr.write(|w| {
-//         w.odr0().set_bit()
-//     });
-//     //=== Port C=====
-//     gpioc.moder.modify(|_,w| w.moder0().output());
-//     gpioc.odr.write(|w| {
-//         w.odr0().set_bit()
-//     });
-//     //=== Port D=====
-//     gpiod.moder.modify(|_,w| w.moder0().output());
-//     gpiod.odr.write(|w| {
-//         w.odr0().set_bit()
-//     });
-//     //==== Port E ====
-//     gpioe.moder.modify(|_,w| w.moder9().output());
-//     gpioe.odr.write(|w| {
-//         w.odr9().set_bit()
-//     });
-//     gpioe.moder.modify(|_,w| w.moder11().output());
-//     gpioe.odr.write(|w| {
-//         w.odr11().set_bit()
-//     });
-//     //=== Port F=====
-//     gpiof.moder.modify(|_,w| w.moder9().output());
-//     gpiof.odr.write(|w| {
-//         w.odr9().set_bit()
-//     });
-
-//     loop{
-    
-//         // if  gpioa.idr.read().idr0().bit_is_clear(){
-//             // let ms = 50_u8;
-//             //     for curr in 0..8 {
-                    
-//             //         let next = (curr + 1) % 8;
-        
-//             //         leds[next].on();
-//             //         delay.delay_ms(ms);
-//             //         leds[curr].off();
-//             //         delay.delay_ms(ms);
-//             //     }
-//             //  }
-//             //  else 
-//             //  if
-//             //  gpioa.idr.read().idr0().bit_is_set(){
-//                 //  for i in 0..8
-//                 //  {
-//                     //  leds[i].off();
-//                 //  }
-//             //  }
-            
+//             leds[next].on();
+//             delay.delay_ms(ms);
+//             leds[curr].off();
+//             delay.delay_ms(ms);
+//         }
 //     }
 // }
+
+//========= using input ===================== 
+#![no_main]
+#![no_std]
+
+use aux5::{entry, prelude::*};
+
+#[entry]
+fn main() -> ! {
+    let (mut delay,gpioa, gpiob, gpioc, gpiod, gpioe, gpiof, rcc) = aux5::init();
+
+    rcc.ahbenr.modify(|_, w| {
+        w.iopaen().set_bit();
+        w.iopben().set_bit();
+        w.iopcen().set_bit();
+        w.iopden().set_bit();
+        w.iopeen().set_bit();
+        w.iopfen().set_bit()
+
+    });
+    gpiob.moder.modify(|_,w| w.moder0().output());
+    gpiob.odr.write(|w| {
+        w.odr0().set_bit()
+    });
+    //=== Port C=====
+    gpioc.moder.modify(|_,w| w.moder0().output());
+    gpioc.odr.write(|w| {
+        w.odr0().set_bit()
+    });
+    //=== Port D=====
+    gpiod.moder.modify(|_,w| w.moder0().output());
+    gpiod.odr.write(|w| {
+        w.odr0().set_bit()
+    });
+    //==== Port E ====
+    gpioe.moder.modify(|_,w| w.moder9().output());
+    gpioe.odr.write(|w| {
+        w.odr9().set_bit()
+    });
+    gpioe.moder.modify(|_,w| w.moder11().output());
+    gpioe.odr.write(|w| {
+        w.odr11().set_bit()
+    });
+    //=== Port F=====
+    gpiof.moder.modify(|_,w| w.moder9().output());
+    gpiof.odr.write(|w| {
+        w.odr9().set_bit()
+    });
+
+    loop{
+    
+        // if  gpioa.idr.read().idr0().bit_is_clear(){
+            // let ms = 50_u8;
+            //     for curr in 0..8 {
+                    
+            //         let next = (curr + 1) % 8;
+        
+            //         leds[next].on();
+            //         delay.delay_ms(ms);
+            //         leds[curr].off();
+            //         delay.delay_ms(ms);
+            //     }
+            //  }
+            //  else 
+            //  if
+            //  gpioa.idr.read().idr0().bit_is_set(){
+                //  for i in 0..8
+                //  {
+                    //  leds[i].off();
+                //  }
+            //  }
+            
+    }
+}
 
 // #![no_main]
 // #![no_std]
